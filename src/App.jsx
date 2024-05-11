7694b57e9b303dff23f3436b0fc85b8e00093fe4
ngan <vutran@sheridancollege.ca>
2024-05-11 12:57:00 -0400

    Material UI Installation

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
-function App() {
+import { Container, Typography } from "@material-ui/core"
 
+function App() {
+  return (
+    <Container>
+      <Typography variant="h1" align="center" color="primary">Hello World</Typography>
+    </Container>
+  )
 }
 
 export default App
+
+
+
+
+// Solution to the error of Material UI Installation: https://stackoverflow.com/questions/72596908/could-not-resolve-dependency-error-peer-react16-8-0-17-0-0-from-materia

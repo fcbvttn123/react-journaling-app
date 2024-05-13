9c25b16c475a48fda0157f8ca136faaa91874499
ngan <vutran@sheridancollege.ca>
2024-05-13 09:55:10 -0400

    Working on READ operation, Card UI is Done

---
+++ src/components/Card.jsx
@@ -0,0 +1,50 @@
+import { Avatar, Card, CardContent, CardHeader, Typography, makeStyles } from "@material-ui/core";
+import { red } from '@material-ui/core/colors';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        maxWidth: 345,
+    },
+    media: {
+        height: 0,
+        paddingTop: '56.25%',
+    },
+    expand: {
+        transform: 'rotate(0deg)',
+        marginLeft: 'auto',
+        transition: theme.transitions.create('transform', {
+        duration: theme.transitions.duration.shortest,
+        }),
+    },
+    expandOpen: {
+        transform: 'rotate(180deg)',
+    },
+    avatar: {
+        backgroundColor: red[500],
+    },
+}));
+
+export function JournalCard({avatar, title, date, content}) {
+
+    const classes = useStyles();
+
+    function capitalizeFirstLetter(str) {
+        return str.charAt(0).toUpperCase() + str.slice(1);
+    }
+
+    return (
+        <Card className={classes.root} elevation={6}>
+            <CardHeader 
+                avatar={<Avatar className={classes.avatar}>{avatar}</Avatar>} 
+                title={`${capitalizeFirstLetter(title)} Day`}
+                subheader={date}
+            />
+            <CardContent>
+                <Typography variant="body2" color="textSecondary" component="p">
+                    {content}
+                </Typography>
+            </CardContent>
+        </Card>
+    )
+
+}
--- src/pages/journals/AllJournals.jsx
+++ src/pages/journals/AllJournals.jsx
@@ -1,5 +1,19 @@
+import { Grid } from "@material-ui/core";
+import { JournalCard } from "../../components/Card";
+import { useState } from "react"
+
+const localStorageKey = "react-journal-app"
+
 export function AllJournals() {
+    const [allJournals, setAllJournals] = useState(JSON.parse(localStorage.getItem(localStorageKey)))
+    function capitalizeFirstLetter(str) {
+        return str.charAt(0).toUpperCase() + str.slice(1);
+    }
     return (
-        <h1>All Journals</h1>
+        <div className="m-4">
+            <Grid container spacing={3} justifyContent="center">
+                {allJournals.map((e, i) => <Grid key={i} item xs={12} sm={6} lg={4}><JournalCard avatar={"F"} title={e.daySummary} date={e.date} content={e.description} /></Grid>)}
+            </Grid>
+        </div>
     )
 }

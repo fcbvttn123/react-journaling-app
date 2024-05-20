6561e2be7d07d743810ab396e1bfab8697e5ff91
ngan <vutran@sheridancollege.ca>
2024-05-20 15:27:00 -0400

    User Email is added when user submits a Journal

--- src/pages/create-journal/handleFormSubmit.js
+++ src/pages/create-journal/handleFormSubmit.js
@@ -1,7 +1,11 @@
 import { useAddToCollection } from "../../hooks/addCollection"
 import { journalsCollection } from "../../config/firebase"
+import { localStorageKeyForAccountInfo } from "../../App"
 
 export async function useHandleFormSubmit(formSubmitEvent, data) {
     formSubmitEvent.preventDefault()
-    useAddToCollection(journalsCollection, data)
+    useAddToCollection(journalsCollection, {
+        ...data, 
+        email: JSON.parse(localStorage.getItem(localStorageKeyForAccountInfo)).email
+    })
 }

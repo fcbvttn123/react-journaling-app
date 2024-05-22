3d1d85540942902204082c1e14362e00204bef50
ngan <vutran@sheridancollege.ca>
2024-05-21 21:08:08 -0400

    Extract Form to a separate Component

--- src/pages/create-journal/CreateJournal.jsx
+++ src/pages/create-journal/CreateJournal.jsx
@@ -1,61 +1,11 @@
-import { Button, Container, FormControl, FormControlLabel, FormLabel, Radio, RadioGroup, TextField, Typography, makeStyles } from "@material-ui/core";
-import { format } from "date-fns";
-import { useState } from "react";
-import { useHandleFormItemChange } from "./handleFormItemChange";
-import { useHandleFormSubmit } from "./handleFormSubmit";
-
-const useStyles = makeStyles((theme) => ({
-  textField: {
-    marginLeft: theme.spacing(1),
-    marginRight: theme.spacing(1),
-    width: 200,
-  },
-}));
+import { Container, Typography } from "@material-ui/core";
+import { Form } from "./Form";
 
 export function CreateJournal() {
-
-  const classes = useStyles();
-
-  const [formData, setFormData] = useState({
-    date: format(new Date(), "yyyy-MM-dd"),
-    description: "",
-    daySummary: "",
-  });
-
   return (
-
-    <Container>
-
-      <div className="h-24 w-full"></div>
-
-      {/* Header Text */}
-      <Typography variant="h4" component="h1" color="primary" name="date" value={formData.date} gutterBottom>
-        Create Your Today's Journal
-      </Typography>
-
-      {/* Form */}
-      <form onSubmit={(e) => useHandleFormSubmit(e, formData)}>
-        <TextField id="date" name="date" label="Date" type="date" color="primary" onChange={(e) => setFormData(useHandleFormItemChange(e, formData))} defaultValue={formData.date} className={classes.textField} />
-        <br />
-        <br />
-        <TextField onChange={(e) => setFormData(useHandleFormItemChange(e, formData))} id="outlined-multiline-static" name="description" label="How was your day?" color="primary" variant="filled" value={formData.description} minRows={7} multiline required fullWidth />
-        <br />
-        <br />
-        <FormControl component="fieldset">
-          <FormLabel component="legend" color="primary"> Your day summary </FormLabel>
-          <RadioGroup onChange={(e) => setFormData(useHandleFormItemChange(e, formData))} name="daySummary" color="primary" >
-            <FormControlLabel value="bad" control={<Radio color="primary" />} label="Bad" color="primary" />
-            <FormControlLabel value="normal" label="Normal" control={<Radio color="primary" />} color="primary" />
-            <FormControlLabel value="good" control={<Radio color="primary" />} label="Good" color="primary" />
-          </RadioGroup>
-        </FormControl>
-        <br />
-        <br />
-        <Button variant="contained" color="primary" type="submit"> Submit </Button>
-      </form>
-
+    <Container className="mt-20">
+      <Typography variant="h4" component="h1" color="primary" gutterBottom>Create Your Today's Journal</Typography>
+      <Form />
     </Container>
-
   );
-
 }
---
+++ src/pages/create-journal/Form.jsx
@@ -0,0 +1,63 @@
+import { useHandleFormSubmit } from "./handleFormSubmit"
+import { useHandleFormItemChange } from "./handleFormItemChange";
+import { useState } from "react";
+import { format } from "date-fns";
+import { Button, FormControl, FormControlLabel, FormLabel, Radio, RadioGroup, TextField, makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  textField: {
+    marginLeft: theme.spacing(1),
+    marginRight: theme.spacing(1),
+    width: 200,
+  },
+}));
+
+export function Form() {
+    const classes = useStyles();
+    const [formData, setFormData] = useState({
+        date: format(new Date(), "yyyy-MM-dd"),
+        description: "",
+        daySummary: "",
+    });
+    return (
+        <form onSubmit={(e) => useHandleFormSubmit(e, formData)}>
+            <TextField 
+                id="date" 
+                name="date" 
+                label="Date" 
+                type="date" 
+                color="primary" 
+                defaultValue={formData.date} className={classes.textField} 
+                onChange={(e) => setFormData(useHandleFormItemChange(e, formData))} 
+            />
+            <br />
+            <br />
+            <TextField 
+                multiline 
+                required 
+                fullWidth 
+                id="outlined-multiline-static" 
+                name="description" 
+                label="How was your day?" 
+                color="primary" 
+                variant="filled" 
+                minRows={7} 
+                value={formData.description} 
+                onChange={(e) => setFormData(useHandleFormItemChange(e, formData))} 
+            />
+            <br />
+            <br />
+            <FormControl component="fieldset">
+                <FormLabel component="legend" color="primary"> Your day summary </FormLabel>
+                <RadioGroup onChange={(e) => setFormData(useHandleFormItemChange(e, formData))} name="daySummary" color="primary" >
+                    <FormControlLabel value="bad" control={<Radio color="primary" />} label="Bad" color="primary" />
+                    <FormControlLabel value="normal" label="Normal" control={<Radio color="primary" />} color="primary" />
+                    <FormControlLabel value="good" control={<Radio color="primary" />} label="Good" color="primary" />
+                </RadioGroup>
+            </FormControl>
+            <br />
+            <br />
+            <Button variant="contained" color="primary" type="submit"> Submit </Button>
+        </form>
+    )
+}
